25ec5ba26c3ab45d9b0ddff948c452668a892b0f
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2025-06-06 09:27:14 -0700

    feat(front page): style leaderboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import LeaderboardFront from "./ui/leaderboard-front/leaderboard-front";
 
 export default function Home() {
   return (
-    <div>
-      <LeaderboardFront />
+    <div className="grid grid-cols-12 gap-2">
+      <div className="col-span-3">
+        <LeaderboardFront />
+      </div>
+      <div className="col-span-9"></div>
+      
     </div>
      );
 }
--- src/app/ui/leaderboard-front/leaderboard-front.tsx
+++ src/app/ui/leaderboard-front/leaderboard-front.tsx
@@ -20,14 +20,50 @@ export default function LeaderboardFront() {
 
     console.log('data', data);
 
+    const colors = [
+        "text-fuchsia-200",
+        "text-indigo-200",
+        "text-cyan-200",
+        "text-emerald-200",
+        "text-lime-200",
+        "text-amber-200",
+        "text-red-200"
+    ]
+    const tiers = [
+        0.3,
+        0.45,
+        0.6,
+        0.75,
+        0.85,
+        0.9
+    ]
+
+    function getColor(ratio:number): string {
+        if (!ratio) return "";
+        if ((1 - ratio) < tiers[0]) {
+            return colors[0];
+        } else if ((1 - ratio) < tiers[1]) {
+            return colors[1];
+        } else if ((1 - ratio) < tiers[2]) {
+            return colors[2];
+        } else if ((1 - ratio) < tiers[3]) {
+            return colors[3];
+        } else if ((1 - ratio) < tiers[4]) {
+            return colors[4];
+        } else if ((1 - ratio) < tiers[5]) {
+            return colors[5];
+        } else
+        return colors[6];
+    }
+
     return (
-        <div>
+        <div className="p-5">
             { data.length === 0 ? <div /> : (
                 <div className='flex flex-col'>
-                    {data[0].goal}
-                    {data[0].rankings.map(rank => {
+                    <p className="text-center font-bold text-xl m-5">{data[0].goal}</p>
+                    {data[0].rankings.map((rank, index) => {
                         return (
-                            <div className='flex flex-between w-7xl'>
+                            <div className={`flex justify-between ${getColor((index + 1) / data[0].rankings.length)}`} key={`${rank.user.name}`}>
                                 <p>{rank.user.name}</p>
                                 <p>{rank.score}</p>
                             </div>
