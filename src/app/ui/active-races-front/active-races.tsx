d22c401e14fcbcf8e79eaaa61373377f7036026a
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2025-06-06 18:45:13 -0700

    feat(front-active): display active race data

--- src/app/ui/active-races-front/active-races.tsx
+++ src/app/ui/active-races-front/active-races.tsx
@@ -22,6 +22,21 @@ export default function ActiveRacesFront() {
     return (
         <div>
             <h2 className="font-bold text-xl text-center m-5">Active Races</h2>
+            <div className="flex flex-col">
+                {data.map(race => {
+                    return (
+                        <div key={race.name} className='flex flex-col'>
+                            <p className="text-xl font-bold text-yellow-200">{race.name.slice(6)}</p>
+                            <div className="flex">
+                                <p className="mr-10">Entrants: {race.entrants_count}</p>
+                                <p>Opened at: {new Date(race.opened_at).toLocaleTimeString()}</p>
+                            </div>
+                            <p>Status: {race.status.verbose_value}</p>
+                        </div>
+                        
+                    );
+                })}
+            </div>
         </div>
     )
 }
