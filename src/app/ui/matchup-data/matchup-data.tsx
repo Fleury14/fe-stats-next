738114cd664676dfe201f13348016f15cc385650
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2025-10-10 15:34:55 -0700

    feat(matchup data): display h2h race links

--- src/app/ui/matchup-data/matchup-data.tsx
+++ src/app/ui/matchup-data/matchup-data.tsx
@@ -4,6 +4,7 @@ import { fetchTLH2H } from '@/app/lib/data/fetch-tellah-data';
 import { TLHead2HeadRace } from '@/app/lib/interfaces';
 import { useState, useEffect} from 'react';
 import ParseMatchupData from './parse-matchup';
+import Link from 'next/link';
 
 export default function MatchupData({ id1, id2}: {id1: string, id2: string}) {
 
@@ -41,6 +42,14 @@ export default function MatchupData({ id1, id2}: {id1: string, id2: string}) {
                     <div className="flex flex-col">
                         <p>{parsedMatchup.wins2}</p>
                     </div>
+                </div>
+                <div className="flex flex-col m-5 items-center">
+                    {results.map(result => {
+                        return (
+                            <Link className="text-blue-400 hover:bg-slate-800" key={result.raceId} href={`/race/${result.roomName}`}>{result.roomName}</Link>
+                            // <p key={result.raceId}>{result.roomName}</p>
+                        );
+                    })}
                 </div>
             </div>
         );
